6eb169373d3749b4af7ac03e9002716f7b9d12f8
NoSegfault <development.my6565@gmail.com>
2021-05-22 16:11:41 -0400

    Reconnect wifi when disconnected, and removed max retry

--- main/wifi.c
+++ main/wifi.c
@@ -14,30 +14,21 @@
 #define TAG CONFIG_LOGGING_TAG
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
-#define MAX_RETRY 10
 
 // FreeRTOS event group to signal when we are connected
 static EventGroupHandle_t wifi_event_group;
 
 static void event_handler(void* arg, esp_event_base_t event_base,
                           int32_t event_id, void* event_data) {
-  static int retry_num = 0;
   if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
     esp_wifi_connect();
   } else if (event_base == WIFI_EVENT &&
              event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    if (retry_num < MAX_RETRY) {
-      esp_wifi_connect();
-      retry_num++;
-      ESP_LOGI(TAG, "Retry to connect to the AP");
-    } else {
-      xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
-    }
-    ESP_LOGI(TAG, "Connect to the AP failed");
+    esp_wifi_connect();
+    ESP_LOGI(TAG, "Retry to connect to the AP");
   } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
     ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
     ESP_LOGI(TAG, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-    retry_num = 0;
     xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
   }
 }
@@ -83,17 +74,11 @@ esp_err_t wifi_init_sta(void) {
   if (bits & WIFI_CONNECTED_BIT) {
     ESP_LOGI(TAG, "Connected to SSID: %s", CONFIG_WIFI_SSID);
   } else if (bits & WIFI_FAIL_BIT) {
-    ESP_LOGI(TAG, "Failed to connect to SSID: %s, Password: %s",
+    ESP_LOGD(TAG, "Failed to connect to SSID: %s, Password: %s",
              CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
   } else {
     ESP_LOGE(TAG, "UNEXPECTED EVENT");
   }
-
-  ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                               &event_handler));
-  ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                               &event_handler));
-  vEventGroupDelete(wifi_event_group);
 
   return ESP_OK;
 }
